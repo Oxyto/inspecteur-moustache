6c3b72f1307424c5f2fbde77dd82c42adb3c58f5
Louis De Macedo <louis.de.macedo.ldm@gmail.com>
2023-11-12 16:02:06 +0100

    feat(scenes): change scene place

--- scripts/buttons/StartButton.gd
+++ scripts/buttons/StartButton.gd
@@ -1,15 +1,6 @@
 extends Button
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+const SCENE_PATH: String = "res://scenes/MainGame.tscn"
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_game/game.tscn")
+	get_tree().change_scene_to_file(SCENE_PATH)
